688e824d5117c5b9dd0be425b6f89dbfd4036dcf
Nayeem M. Muzahid <nayeemm.muzahid@gmail.com>
2019-04-11 07:51:52 +0600

    Pracitce code of basic javascripts

---
+++ custom.js
@@ -0,0 +1,14 @@
+var firstName = 'Nayeem',
+    lastName = 'M. Muzahid';
+
+    console.log(firstName + " "+lastName);
+
+
+    var num = 10;
+    console.log(num);
+
+    var a;
+    console.log(a);
+
+    var b = null;
+    console.log(b);
