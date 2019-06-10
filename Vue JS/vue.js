11c40c054f73937062c96e8dc277f7806ae7c76a
Nayeem M. Muzahid <nayeemm.muzahid@gmail.com>
2019-06-11 00:41:17 +0600

    New code added

--- Vue JS/vue.js
+++ Vue JS/vue.js
@@ -1,17 +1,29 @@
 new Vue({
-    el: '.app',
-    data: {
-        title: 'Hello world',
-        link: 'http://google.com'
+  el: '.app',
+  data: {
+    title: 'Hello world',
+    link: 'http://google.com',
+    counter: 0,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    changeTitle: function(event) {
+      this.title = event.target.value;
     },
-    methods: {
-        changeTitle: function(event) {
-            this.title = event.target.value;
-        },
-        sayHello: function(){
-            this.title = 'Hello';
-            return 'Hello';
-        }
-
+    sayHello: function() {
+      this.title = 'Hello';
+      return 'Hello';
+    },
+    increaseCounter: function(step, event) {
+      this.counter += step;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function() {
+      alert('You Sure?');
     }
+  }
 });
